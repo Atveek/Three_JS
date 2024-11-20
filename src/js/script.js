00e3b76b6fa532a3c35d1e044a867943fe6e0fac
Atveek <atveekdungarani2021@gmail.com>
2024-11-20 15:34:12 +0530

    add gui in sphere for changes of animation , color and wireframe by user

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,7 @@ const renderer = new THREE.WebGLRenderer();
 
 //define the size of the screen when 3D element is render
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
 
 //append that renderer into the html file
 document.body.appendChild(renderer.domElement);
@@ -51,13 +52,14 @@ scene.add(box);
 
 //plane
 const planeGeometry = new THREE.PlaneGeometry(30, 30);
-const planeMaterial = new THREE.MeshBasicMaterial({
+const planeMaterial = new THREE.MeshStandardMaterial({
   color: 0xffffff,
   side: THREE.DoubleSide,
 });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(plane);
 plane.rotation.x = -0.5 * Math.PI;
+plane.receiveShadow = true;
 
 //gridHelper
 const gridHelper = new THREE.GridHelper(30);
@@ -65,30 +67,53 @@ scene.add(gridHelper);
 
 //sphereGeometry - radius , horizontal round division ,vertical division
 const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
-const sphereMaterial = new THREE.MeshBasicMaterial({
+const sphereMaterial = new THREE.MeshStandardMaterial({
   color: 0x0000ff,
   wireframe: false,
 });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
-
 sphere.position.set(-10, 10, 0);
+sphere.castShadow = true;
 
+const ambientLight = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(ambientLight);
+
+//direction of light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 11);
+scene.add(directionalLight);
+directionalLight.position.set(-30, 50, 0);
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.top = 12
+directionalLight.shadow.camera.bottom = -12
+
+const DirectionalLightHelper = new THREE.DirectionalLightHelper(
+  directionalLight,
+  5
+);
+scene.add(DirectionalLightHelper);
+
+const DLightShadowHelper = new THREE.CameraHelper(
+  directionalLight.shadow.camera
+);
+scene.add(DLightShadowHelper);
+
+//define GUI
+//define option -  the property of object which change by user
+//add that property in screen of user
 const gui = new dat.GUI();
-
 const options = {
   sphereColor: "#ffea00",
   wireframe: false,
   speed: 0.01,
 };
-
 gui.addColor(options, "sphereColor").onChange(function (e) {
   sphere.material.color.set(e);
 });
 gui.add(options, "wireframe").onChange(function (e) {
   sphere.material.wireframe = e;
 });
-gui.add(options, "speed", 0.01, 0.1);
+gui.add(options, "speed", 0, 0.1);
 
 let step = 0;
 
